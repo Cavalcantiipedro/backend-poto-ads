e45284ab07a9cc292f7196a68bcc3dd6de91ba65
Cavalcantiipedro <pedrocavalcanti.r@gmail.com>
2024-11-07 11:11:37 -0300

    aula dia 7-11

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ let app = express();
 
 const port = 3000;
 
+const sequelize = require(`./config/db`);
+const Usuario = require('./models/usuario');
+const Ponto = require('./models/ponto');
+
+sequelize.sync({alter: true})
+.then(() => {
+    console.log("sync feito com sucesso");
+})
+.catch(error => {console.log("deu erro!"+ error)
+});
+
+const pedro = Usuario.create({nome:'Pedro',email:'pedrocavalcanti.r@gmail.com',login:'Pedro',senha:'123456'});
 
 app.get("/user/:id1-:id2", (req,res) => {
     console.log(req.params);
